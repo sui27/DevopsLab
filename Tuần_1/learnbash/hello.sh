f00d0e1d157c213bfef1c47f24b28764f5386dc3
Nguyễn Bình <107705362+sui27@users.noreply.github.com>
2025-04-18 11:02:03 +0700

    Update hello.sh

--- Tuần_1/learnbash/hello.sh
+++ Tuần_1/learnbash/hello.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 echo "hello devops!"
 
+name="Devops"
+
+echo "Hello $Name"
 :'
 !/bin/bash: Shebang, bảo Bash chạy script này.
 chmod +x: Cấp quyền thực thi.
-./hello.sh: Chạy file.'                   
+./hello.sh: Chạy file.
+name là tên của biến trong bash
+
+run: ./hello.sh --> output : hello devops! 
+                             Hello Devops
+'                   
